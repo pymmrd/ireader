523562afd1a2ec1e71512414bdc0dce3e9e0aa91
pymmrd <b.zougang@gmail.com>
2013-08-15 18:10:36 +0800

    print miss

--- ireader/bin/print_miss.py
+++ ireader/bin/print_miss.py
@@ -14,6 +14,17 @@ sys.path.append(PROJECT_PATH)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ireader.settings'
 
 from book.models import Book, BookItem, BookPart, Category
+from book.klass import get_bookitem_model
 
-def update_content():
-	pass
+def print_miss():
+	book = Book.objects.values_list('id', flat=True).all()
+	for b in book:
+		cls = get_bookitem_model(b)
+		count = cls.objects.filter(book__id=b).count()
+		if count == 0:
+			print b
+
+if __name__ == "__main__":
+	print_miss()
+
+
