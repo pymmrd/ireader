0da419a401d48d3cfa5ecebac7637c0e51da40a8
pymmrd <b.zougang@gmail.com>
2013-08-24 16:49:13 +0800

    remove brand

--- ireader/ireader/settings.py
+++ ireader/ireader/settings.py
@@ -153,6 +153,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 	'book',
+	'crawler',
 )
 
 # A sample logging configuration. The only tangible logging
