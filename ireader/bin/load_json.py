2c8414fcdbc7c8536f00c3671014658e086b4bc9
zg163 <zg163@zg163-Lenovo-IdeaPad-Y470.(none)>
2013-09-01 08:22:48 +0800

    load json

--- ireader/bin/load_json.py
+++ ireader/bin/load_json.py
@@ -4,6 +4,7 @@ import os
 import sys
 import json
 import glob
+import shutil
 import threadpool
 from datetime import datetime
 
@@ -12,7 +13,9 @@ abspath = os.path.abspath
 dirname = os.path.dirname
 CURRENT_PATH = abspath(dirname(__file__))
 PROJECT_PATH = abspath(dirname(CURRENT_PATH))
-DATA_PATTERN = "/var/www/wwwroot/ireader/pybook/new_data/*.json"
+DATA_PATTERN = "/home/zg163/djcode/ireader/pybook/new_data/*.json"
+DEST_PATH = '/home/zg163/data/'
+SRC_PATH = '/home/zg163/pdf/data/book/'
 
 sys.path.append(PROJECT_PATH)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ireader.settings'
@@ -20,15 +23,15 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'ireader.settings'
 from book.models import Book, BookItem, BookPart, Category
 
 CATEGORY_MAP = {
-    'magic': 1,
-    'sord': 2,
-    'dushi': 3,
-    'lover': 4,
-    'time_travel': 5,
-    'game': 6,
-    'monster': 7,
-    'science': 8,
-    'other': 9,
+    'magic': (1, 'book1'),
+    'sord': (2, 'book2'),
+    'dushi': (3, 'book3'),
+    'lover': (4, 'book4'),
+    'time_travel': (5, 'book5'),
+    'game': (6, 'book6'),
+    'monster': (7, 'book7'),
+    'science': (8, 'book8'),
+    'other': (9, 'book9'),
 }
 
 #CATEGORY_LIST = list(Category.objects.all().order_by('id')) 
@@ -39,10 +42,13 @@ def load_json(data_path):
     file_name = data_path.rsplit('/', 1)[-1] 
     prefix_name = file_name.rsplit('_', 1)[0]
     try:
-        index = CATEGORY_MAP.get(prefix_name)
+        index, sub_dir = CATEGORY_MAP.get(prefix_name)
     except KeyError:
         print 'error:', data_path
     else:
+        dest_path = os.path.join(DEST_PATH, sub_dir)
+        if not os.path.exists(dest_path):
+            os.makedirs(dest_path)
         category = CATEGORY_LIST[index-1] 
         with open(data_path, 'r') as f:
             try:
@@ -59,21 +65,23 @@ def load_json(data_path):
                     word_num = item.get('word_num')
                     author = item.get('author')
                     name = item.get('name')[:-4]
-                    book = Book()
-                    book.name = name
-                    book.category = category 
-                    book.author = author
-                    book.word_num = word_num
-                    book.save()
+                    book, created = Book.objects.get_or_create(name=name, author=author, defaults={'category': category, 'word_num': word_num})
+                    if not created:
+                        book.save()
+                        src_path = os.path.join(SRC_PATH, name).encode('utf-8')
+                        try:
+                            shutil.move(src_path, dest_path)
+                        except:
+                            pass
 
 if __name__ == "__main__":
     from django.db import connection
-    #for item in glob.iglob(DATA_PATTERN):
-    #    load_json(item)
+    for item in glob.iglob(DATA_PATTERN):
+        load_json(item)
+    """
     pool = threadpool.ThreadPool(10)
     requests = threadpool.makeRequests(load_json, glob.iglob(DATA_PATTERN))
     for req in requests:
         pool.putRequest(req)
     pool.wait()
-    import pprint
-    pprint.pprint(connection.queries)
+    """
