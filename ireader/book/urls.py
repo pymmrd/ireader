99acf915f8d2b55eea690dc9c1c13274b2bb37de
wenshin <wenshin@vps1265.(none)>
2013-11-15 00:24:25 +0800

    feature: 修改首页链接跳转到新建的/book/intro/页面；

--- ireader/book/urls.py
+++ ireader/book/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('book.views',
 	(r'^0(?P<pk>\d?)/((?P<page>\d+)/)?$', 'show_category', {'tmpl': 'book/category.html'}, 'show-category'),
 	(r'^content/(?P<pk>\d+)/$', 'show_content', {'tmpl': 'book/content.html'}, 'show-content'),
-	(r'^intro/(?P<pk>\d+)/$', 'show_content', {'tmpl': 'book/introduction.html'}, 'show-introduction'),
+	(r'^intro/(?P<pk>\d+)/$', 'show_content', {'tmpl': 'book/introduction.html'}, 'show-intro'),
 	(r'^detail/(?P<partition>\d+)/(?P<pk>\d+)/$', 'show_detail', {'tmpl': 'book/detail.html'}, 'show-detail'),
 )
